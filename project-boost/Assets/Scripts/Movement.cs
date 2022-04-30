36e5741f5e4346337e331a1c4c73d734932fff8b
ScottLeeBJJ <scottlee5455@outlook.com>
2022-04-30 10:02:00 -0700

    Adeed upward thrust movement to our rocket.

--- project-boost/Assets/Scripts/Movement.cs
+++ project-boost/Assets/Scripts/Movement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    //rb is commonly Rigidbody shortened.
+    [SerializeField] float mainThrust = 1000f;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("rotate left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("rotate right");
+        }
     }
 }
